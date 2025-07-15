fafd9705b89ad255ef29ad09657c094a69ab75b4
NISHIJIMA Kazuyuki <nishijima@meibinlab.jp>
2025-07-16 08:55:30 +0900

    viteの設定ファイルを追加

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Haori-JS",
+      fileName: (format) => `haori.${format}.js`,
+      formats: ["es", "iife"]
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    },
+    outDir: "dist",
+    emptyOutDir: true
+  },
+  test: {
+    environment: "jsdom"
+  }
+});
