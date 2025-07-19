0a8c7ef186aafb0c3cc8d32357bc66f7030a59e3
NISHIJIMA Kazuyuki <nishijima@meibinlab.jp>
2025-07-20 01:00:25 +0900

    index.tsを更新

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Haori-JS - A lightweight HTML-first UI engine
+ *
+ * HTML要素に対してデータバインディングやリアクティブな更新を提供する
+ * 軽量なUIエンジンのコア機能をエクスポートします。
+ */
+
+export * from './dev';
+export * from './env';
