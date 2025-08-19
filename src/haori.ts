586633ef002b53745bc44f64991574cccfade179
NISHIJIMA Kazuyuki <nishijima@meibinlab.jp>
2025-08-19 09:36:43 +0900

    Haoriクラスを再作成

---
+++ src/haori.ts
@@ -0,0 +1,104 @@
+import Log from './log';
+import Queue from './queue';
+
+/**
+ * Haoriクラスは、アプリケーション全体で使用されるユーティリティメソッドを提供します。
+ * 挙動を変更する場合は必要に応じてオーバライドしてください。
+ */
+export class Haori {
+  /**
+   * アラートダイアログを表示します。
+   *
+   * @param message アラートメッセージ
+   * @returns アラートが閉じられると解決されるPromise
+   */
+  public static alert(message: string): Promise<void> {
+    return Queue.enqueue(() => {
+      window.alert(message);
+    }, true) as Promise<void>;
+  }
+
+  /**
+   * 確認ダイアログを表示します。
+   *
+   * @param message 確認メッセージ
+   * @returns ユーザーがOKをクリックした場合はtrue、キャンセルした場合はfalseが解決されるPromise
+   */
+  public static confirm(message: string): Promise<boolean> {
+    return Queue.enqueue(() => {
+      return window.confirm(message);
+    }, true) as Promise<boolean>;
+  }
+
+  /**
+   * 通知メッセージを表示します。
+   *
+   * @param message 通知メッセージ
+   * @param level メッセージのレベル（'info' | 'warning' | 'error'）
+   */
+  public static message(
+    message: string,
+    level: 'info' | 'warning' | 'error',
+  ): Promise<void> {
+    return Haori.alert(`[${level.toUpperCase()}] ${message}`);
+  }
+
+  /**
+   * ダイアログを開きます。
+   *
+   * @param element 開くダイアログのHTML要素
+   */
+  public static openDialog(element: HTMLElement): Promise<unknown> {
+    return Queue.enqueue(() => {
+      if (element instanceof HTMLDialogElement) {
+        element.showModal();
+      } else {
+        Log.error('[Haori]', 'Element is not a dialog: ', element);
+      }
+    }, true);
+  }
+
+  /**
+   * ダイアログを閉じます。
+   *
+   * @param element 閉じるダイアログのHTML要素
+   */
+  public static closeDialog(element: HTMLElement): Promise<unknown> {
+    return Queue.enqueue(() => {
+      if (element instanceof HTMLDialogElement) {
+        element.close();
+      } else {
+        Log.error('[Haori]', 'Element is not a dialog: ', element);
+      }
+    }, true);
+  }
+
+  /**
+   * エラーメッセージを追加します。
+   *
+   * @param target メッセージを表示する要素
+   * @param message エラーメッセージ
+   */
+  public static addErrorMessage(
+    target: HTMLElement | HTMLFormElement,
+    message: string,
+  ): Promise<unknown> {
+    return Queue.enqueue(() => {
+      target.parentElement?.setAttribute('data-message', message);
+    }, true);
+  }
+
+  /**
+   * メッセージをクリアします。
+   *
+   * @param parent メッセージをクリアする親要素
+   */
+  public static clearMessages(parent: HTMLElement): Promise<unknown> {
+    return Queue.enqueue(() => {
+      parent.removeAttribute('data-message');
+      parent.querySelectorAll('[data-message]').forEach(element => {
+        element.removeAttribute('data-message');
+      });
+    }, true);
+  }
+}
